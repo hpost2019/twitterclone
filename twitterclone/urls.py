7d9211a440ba60ce24606c1288c941ef29f4e366
Howard Post <hpost2012@gmail.com>
2020-09-01 05:48:07 -0700

    added authentication and new user signup

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authentication import views as authviews
+from tweet import views as tweetviews
 
 urlpatterns = [
+    path('', tweetviews.index_view, name="home"),
+    path('login/', authviews.login_view),
+    path('signup/', authviews.signup_view),
+    path('logout/', authviews.logout_view),
     path('admin/', admin.site.urls),
 ]
